0d65444b6934633746685aeb10a24813e07c1f42
windstormeye <877302410@qq.com>
2018-11-21 14:10:24 +0800

    v-model

--- 0/index.js
+++ 0/index.js
@@ -2,10 +2,5 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello, PJHubs!'
-    },
-    methods: {
-        reverseMessage: function() {
-            this.message = this.message.split('').reverse().join('')
-        } 
-    }   
+    }
 });
